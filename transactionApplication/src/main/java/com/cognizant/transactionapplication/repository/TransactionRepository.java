1c127ddce5928cfdad3b5bd9475d3428f9b42aa6
VyanketeshNagpure <112158595+VyanketeshNagpure@users.noreply.github.com>
2023-05-12 18:36:36 +0530

    Delete transactionApplication directory

--- transactionApplication/src/main/java/com/cognizant/transactionapplication/repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.transactionapplication.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognizant.transactionapplication.model.Transaction;
-
-public interface TransactionRepository extends JpaRepository<Transaction, String>{
-
-}
