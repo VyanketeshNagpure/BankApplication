0681dbe80854d8838f4e9a61c8e4a5b9962f8da4
VyanketeshNagpure <112158595+VyanketeshNagpure@users.noreply.github.com>
2023-05-17 14:58:51 +0530

    added swagger depenedency

--- bankapplication/src/main/java/com/cognizant/bankapplication/configuration/SwaggerConfig.java
+++ bankapplication/src/main/java/com/cognizant/bankapplication/configuration/SwaggerConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-	
-	 
+	/*
+	 swagger ui uri available at
+	 http://localhost:8082/swagger-ui.html
+	 */
 }
