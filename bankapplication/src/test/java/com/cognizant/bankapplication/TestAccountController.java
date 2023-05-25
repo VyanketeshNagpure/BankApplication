eee69d13aaf58af423c5e8952698a2daad523cfc
VyanketeshNagpure <112158595+VyanketeshNagpure@users.noreply.github.com>
2023-05-25 18:29:36 +0530

    Update TestAccountController.java

--- bankapplication/src/test/java/com/cognizant/bankapplication/TestAccountController.java
+++ bankapplication/src/test/java/com/cognizant/bankapplication/TestAccountController.java
@@ -1,12 +1,13 @@
 package com.cognizant.bankapplication.controller;
 
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.math.BigInteger;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,10 +48,26 @@ public class TestAccountController {
 	}
 	
 	@Test
+	void testCreateAccountWithBadInput() throws Exception {
+		
+		mockMvc.perform(post("/account").content("{\r\n"
+				+ "    \"customerName\": \"su\",\r\n"
+				+ "    \"permanentAccountNumber\": \"ABDF1234R\",\r\n"
+				+ "    \"dateOfBirth\":\"2001-01-01\",\r\n"
+				+ "    \"AccountType\":\"savings\",\r\n"
+				+ "    \"accountStatus\":\"open\",\r\n"
+				+ "    \"accountBalance\":\"20000\",\r\n"
+				+ "    \"phoneNumber\":\"9011592901\",\r\n"
+				+ "    \"emailId\":\"prashantdhainje1998@gmail.com\"\r\n"
+				+ "}").contentType(MediaType.APPLICATION_JSON))
+			   .andExpect(status().isBadRequest());
+	}
+	
+	@Test
 	void testGetAccount() throws Exception {
 		
 		Account account = new Account();
-		account.setAccountId(BigInteger.valueOf(12768609));
+		account.setAccountId(BigInteger.valueOf(1276860007));
 		account.setAccountBalance(20000d);
 		account.setAccountStatus("open");
 		account.setAccountType(accountType.NonSalary);
@@ -59,10 +76,10 @@ public class TestAccountController {
 		account.setEmailId("test@gmail.com");
 		account.setPhoneNumber("9011426749");
 		account.setPermanentAccountNumber("CKQPN0467T");
-		
-		when(accountRepository.findByAccountId(anyLong())).thenReturn(account);
-		
-		mockMvc.perform(get("/account/"+BigInteger.valueOf(12768609))).andExpect(status().isNotFound());
+	
+		when(accountRepository.findById(any()))
+			.thenReturn(Optional.of(account));
+		mockMvc.perform(get("/account/"+BigInteger.valueOf(127686))).andExpect(status().isFound());
 	}
 
 }
