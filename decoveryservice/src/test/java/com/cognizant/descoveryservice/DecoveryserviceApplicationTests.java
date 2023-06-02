75abe0871b05222bbd7c4afc380c1ed014f5db00
VyanketeshNagpure <112158595+VyanketeshNagpure@users.noreply.github.com>
2023-06-02 14:06:48 +0530

    discoveryService

---
+++ decoveryservice/src/test/java/com/cognizant/descoveryservice/DecoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.descoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
